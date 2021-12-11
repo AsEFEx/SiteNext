df10ba1a7fafc17b4c2d08cf0089e6b1cafe12db
AsEFEx <85035339+AsEFEx@users.noreply.github.com>
2021-12-11 19:04:26 -0300

    Adicionado o restante das páginas

---
+++ src/pages/revista.js
@@ -0,0 +1,77 @@
+
+import React from 'react';
+
+function Revista() {
+    return (
+        <div id="content" className="internas span9">
+            <section id="content-section">
+                <span className="hide">Início do conteúdo da página</span>
+                <h1 className="documentFirstHeading">REVISTA AsEFEx</h1>
+                <p>A revista da AsEFEx tem a finalidade de divulgar matérias nas áreas
+                do ensino e pesquisa em capacitação física e áreas correlatas, desporto,
+                cultura e divulgação de fatos e eventos relacionados à atuação de Ex-Alunos e Amigos,
+                ao CCFEx e suas Organizações Militares Diretamente Subordinadas (EsEFEx, IPCFEx e CDE)
+                e ao Sitio Histórico da Fortaleza de São João. </p>
+                <p><a href="/escreva">Faça sua proposta de matéria para publicação na Revista da AsEFEx.</a></p>
+                <h2>Edições 2021</h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="span4">
+                            <div className=" card">
+                                <a href="https://pt.calameo.com/read/006435344d3a13a215c69?authid=G7WRiQYJICNf"><img src="images/ed114.jpg" />
+                                    <h2><strong>Nr 114</strong></h2>
+                                    <p>Abril e Junho de 2021</p></a>
+                            </div>
+                        </div>
+                        <div className="span4">
+                            <div className=" card">
+                                <a href="https://pt.calameo.com/read/006435344df915ae2ecbd?authid=PiQe6zOM9Ccw"><img src="images/ed113.jpg" />
+                                    <h2><strong>Nr 113</strong></h2>
+                                    <p>Dezembro de 2020</p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <h2>Edições 2020</h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="span4">
+                            <div className=" card">
+                                <a href="https://pt.calameo.com/read/00643534419b2d0f41807?authid=4GXev9E6EqlB"><img src="images/ed112.jpg" />
+                                    <h2><strong>Nr 112</strong></h2>
+                                    <p>Novembro e Dezembro de 2020</p></a>
+                            </div>
+                        </div>
+                        <div className="span4">
+                            <div className=" card">
+                                <a href="https://pt.calameo.com/read/006435344e04bab2bf544?authid=lZcaNNaCa7AH"><img src="images/ed111.jpg" />
+                                    <h2><strong>Nr 111</strong></h2>
+                                    <p>Setembro e Outubro de 2020</p></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="span4">
+                            <div className=" card">
+                                <a href="https://pt.calameo.com/read/0064353445b120d4cc41d?authid=y3M87gniA4Ak"><img src="images/ed110.jpg" />
+                                    <h2><strong>Nr 110</strong></h2>
+                                    <p>Julho e Agosto de 2020</p></a>
+                            </div>
+                        </div>
+                        <div className="span4">
+                            <div className=" card">
+                                <a href="https://pt.calameo.com/read/006435344f44950abb34f?authid=y35kiMz3yHMJ"><img src="images/ed109.jpg" />
+                                    <h2><strong>Nr 109</strong></h2>
+                                    <p>Dezembro de 2019 a Junho de 2020</p></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <span className="hide">Fim do conteúdo da página</span>
+            </section>
+        </div>
+
+    )
+}
+
+export default Revista
