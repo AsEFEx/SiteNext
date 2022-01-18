520614a0e307ec8d3cb9fa497584ef3652e97912
Giulio Bronson <giuliobronson@gmail.com>
2022-01-18 10:32:27 -0300

    Update cadastro.js

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Script from 'next/script'
+import Link from 'next/link'
 import Header from '../components/header'
 import Menu from '../components/menu'
 import Footer from '../components/footer'
@@ -31,10 +32,10 @@ function Cadastro() {
         Learn how to configure a non-root public URL by running `npm run build`. */}
 
         {/* <!--[if lt IE 9]><script src="js/html5shiv.js"></script><![endif]--> */}
-        <link href="images/Logo.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
-        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type='text/css' />
-        <link rel="stylesheet" href="css/template-verde.css" type='text/css' />
-        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type='text/css' />
+        <Link href="images/Logo.ico" rel="shortcut icon" type="image/vnd.microsoft.icon" />
+        <Link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type='text/css' />
+        <Link rel="stylesheet" href="css/template-verde.css" type='text/css' />
+        <Link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type='text/css' />
         {/* <!--[if lt IE 10]><link rel="stylesheet" href="css/ie.css" /><![endif]-->
         <!--[if lt IE 9]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
         <!--[if lt IE 8]><link rel="stylesheet" href="css/ie7.css" /><link rel="stylesheet" href="font-awesome/css/font-awesome-ie7.min.css" /><![endif]-->
@@ -43,8 +44,8 @@ function Cadastro() {
         <!-- chamada de fontes externas --> */}
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,600,800,700' rel='stylesheet'
           type='text/css' />
-        <link rel="stylesheet" href="jplayer/skin/portalpadrao01/jplayer.css" type='text/css' />
-        <link rel="stylesheet" href="css/css_adicional.css" type='text/css' />
+        <Link rel="stylesheet" href="jplayer/skin/portalpadrao01/jplayer.css" type='text/css' />
+        <Link rel="stylesheet" href="css/css_adicional.css" type='text/css' />
       </Head>
 
       <a className="hide" id="topo" href="#accessibility">Ir direto para menu de acessibilidade.</a>
