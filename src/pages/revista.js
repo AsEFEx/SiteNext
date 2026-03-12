34581ac270d21dc04a2809b1829dbb963771c4b5
AsEFEx <site.asefex@gmail.com>
2026-03-12 11:51:42 -0300

    Inclusão da Revista 118, no menu Revistas

--- src/pages/revista.js
+++ src/pages/revista.js
@@ -1,77 +1,135 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Card from '@/components/Card'
+import Head from "next/head";
+import Link from "next/link";
+import Card from "@/components/Card";
 
 export default function Home() {
-  return <>
-    <Head>
-      <title>Revista AsEFEx</title>
-    </Head>
+  return (
+    <>
+      <Head>
+        <title>Revista AsEFEx</title>
+      </Head>
 
-    <section id="content-section">
-      <span className="hide">Início do conteúdo da página</span>
-      <h1 className="documentFirstHeading">REVISTA AsEFEx</h1>
-      <p>A revista da AsEFEx tem a finalidade de divulgar matérias nas áreas
-        do ensino e pesquisa em capacitação física e áreas correlatas, desporto,
-        cultura e divulgação de fatos e eventos relacionados à atuação de Ex-Alunos e Amigos,
-        ao CCFEx e suas Organizações Militares Diretamente Subordinadas (EsEFEx, IPCFEx e CDE)
-        e ao Sitio Histórico da Fortaleza de São João. </p>
-      <p>
-        <Link style={{ color: '#0088CC' }} href="/escreva">Faça sua proposta de matéria para publicação na Revista da AsEFEx.</Link>
-      </p>
+      <section id="content-section">
+        <span className="hide">Início do conteúdo da página</span>
+        <h1 className="documentFirstHeading">REVISTA AsEFEx</h1>
+        <p>
+          A revista da AsEFEx tem a finalidade de divulgar matérias nas áreas do
+          ensino e pesquisa em capacitação física e áreas correlatas, desporto,
+          cultura e divulgação de fatos e eventos relacionados à atuação de
+          Ex-Alunos e Amigos, ao CCFEx e suas Organizações Militares Diretamente
+          Subordinadas (EsEFEx, IPCFEx e CDE) e ao Sitio Histórico da Fortaleza
+          de São João.{" "}
+        </p>
+        <p>
+          <Link style={{ color: "#0088CC" }} href="/escreva">
+            Faça sua proposta de matéria para publicação na Revista da AsEFEx.
+          </Link>
+        </p>
 
-      <h2>Edições 2022</h2>
-      <div className="container">
-        <div className="row">
-          <Card title="Nr 117" image="/images/ed117.jpg" alt="Revista Edição 117" description="Janeiro a Março de 2022"
-            route="https://pt.calameo.com/read/006435344567665a09633?authid=e15gBQsiCu7E" />
+        <h2>Edições 2022</h2>
+        <div className="container">
+          <div className="row">
+            {/*By TC Fabio  */}
+            <Card
+              title="Nr 118"
+              image="/images/ed118.jpg"
+              alt="Revista Edição 118"
+              description="Abril a Junho de 2022"
+              route="https://pt.calameo.com/read/0064353448cc251f10045?authid=mjlUoLQmoa0H"
+            />
 
-          {/* <Card title="Nr ___" image="/images/______.jpg" alt="Revista Edição ___ " description="____ a ____ de 2022"
+            <Card
+              title="Nr 117"
+              image="/images/ed117.jpg"
+              alt="Revista Edição 117"
+              description="Janeiro a Março de 2022"
+              route="https://pt.calameo.com/read/006435344567665a09633?authid=e15gBQsiCu7E"
+            />
+
+            {/* <Card title="Nr ___" image="/images/______.jpg" alt="Revista Edição ___ " description="____ a ____ de 2022"
                         route="https://______.com"/> */}
-        </div>
-      </div>
-
-      <h2>Edições 2021</h2>
-      <div className="container">
-        <div className="row">
-          <Card title="Nr 116" image="/images/ed116.jpg" alt="Revista Edição 116" description="Outubro a Dezembro de 2021"
-            route="https://pt.calameo.com/read/006435344e3ff145ebcc5?authid=IcwOmLTWn2AD" />
-
-          <Card title="Nr 115" image="/images/ed115.jpg" alt="Revista Edição 115" description="Julho a Setembro de 2021"
-            route="https://pt.calameo.com/read/006435344f848ad6cf41a?authid=s9AUDEJk0ONc" />
+          </div>
         </div>
 
-        <div className="row">
-          <Card title="Nr 114" image="/images/ed114.jpg" alt="Revista Edição 114" description="Abril a Junho de 2021"
-            route="https://pt.calameo.com/read/006435344d3a13a215c69?authid=G7WRiQYJICNf" />
+        <h2>Edições 2021</h2>
+        <div className="container">
+          <div className="row">
+            <Card
+              title="Nr 116"
+              image="/images/ed116.jpg"
+              alt="Revista Edição 116"
+              description="Outubro a Dezembro de 2021"
+              route="https://pt.calameo.com/read/006435344e3ff145ebcc5?authid=IcwOmLTWn2AD"
+            />
 
-          <Card title="Nr 113" image="/images/ed113.jpg" alt="Revista Edição 113" description="Dezembro de 2020"
-            route="https://pt.calameo.com/read/006435344df915ae2ecbd?authid=PiQe6zOM9Ccw" />
+            <Card
+              title="Nr 115"
+              image="/images/ed115.jpg"
+              alt="Revista Edição 115"
+              description="Julho a Setembro de 2021"
+              route="https://pt.calameo.com/read/006435344f848ad6cf41a?authid=s9AUDEJk0ONc"
+            />
+          </div>
+
+          <div className="row">
+            <Card
+              title="Nr 114"
+              image="/images/ed114.jpg"
+              alt="Revista Edição 114"
+              description="Abril a Junho de 2021"
+              route="https://pt.calameo.com/read/006435344d3a13a215c69?authid=G7WRiQYJICNf"
+            />
+
+            <Card
+              title="Nr 113"
+              image="/images/ed113.jpg"
+              alt="Revista Edição 113"
+              description="Dezembro de 2020"
+              route="https://pt.calameo.com/read/006435344df915ae2ecbd?authid=PiQe6zOM9Ccw"
+            />
+          </div>
         </div>
-      </div>
 
+        <h2>Edições 2020</h2>
+        <div className="container">
+          <div className="row">
+            <Card
+              title="Nr 112"
+              image="/images/ed112.jpg"
+              alt="Revista Edição 112"
+              description="Novembro a Dezembro de 2020"
+              route="https://pt.calameo.com/read/00643534419b2d0f41807?authid=4GXev9E6EqlB"
+            />
 
-      <h2>Edições 2020</h2>
-      <div className="container">
-        <div className="row">
-          <Card title="Nr 112" image="/images/ed112.jpg" alt="Revista Edição 112" description="Novembro a Dezembro de 2020"
-            route="https://pt.calameo.com/read/00643534419b2d0f41807?authid=4GXev9E6EqlB" />
+            <Card
+              title="Nr 111"
+              image="/images/ed111.jpg"
+              alt="Revista Edição 111"
+              description="Setembro a Outubro de 2020"
+              route="https://pt.calameo.com/read/006435344e04bab2bf544?authid=lZcaNNaCa7AH"
+            />
+          </div>
 
-          <Card title="Nr 111" image="/images/ed111.jpg" alt="Revista Edição 111" description="Setembro a Outubro de 2020"
-            route="https://pt.calameo.com/read/006435344e04bab2bf544?authid=lZcaNNaCa7AH" />
+          <div className="row">
+            <Card
+              title="Nr 110"
+              image="/images/ed110.jpg"
+              alt="Revista Edição 110"
+              description="Julho a Agosto de 2020"
+              route="https://pt.calameo.com/read/0064353445b120d4cc41d?authid=y3M87gniA4Ak"
+            />
+
+            <Card
+              title="Nr 109"
+              image="/images/ed109.jpg"
+              alt="Revista Edição 109"
+              description="Dezembro de 2019 a Junho de 2020"
+              route="https://pt.calameo.com/read/006435344f44950abb34f?authid=y35kiMz3yHMJ"
+            />
+          </div>
         </div>
-
-
-        <div className="row">
-          <Card title="Nr 110" image="/images/ed110.jpg" alt="Revista Edição 110" description="Julho a Agosto de 2020"
-            route="https://pt.calameo.com/read/0064353445b120d4cc41d?authid=y3M87gniA4Ak" />
-
-          <Card title="Nr 109" image="/images/ed109.jpg" alt="Revista Edição 109" description="Dezembro de 2019 a Junho de 2020"
-            route="https://pt.calameo.com/read/006435344f44950abb34f?authid=y35kiMz3yHMJ" />
-        </div>
-
-      </div>
-      <span className="hide">Fim do conteúdo da página</span>
-    </section>
-  </>
+        <span className="hide">Fim do conteúdo da página</span>
+      </section>
+    </>
+  );
 }
