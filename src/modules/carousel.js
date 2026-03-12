918cfb7903b850c4edb0d53f5caf8729f77ed38d
AsEFEx <site.asefex@gmail.com>
2026-03-12 16:44:18 -0300

    Ordenar como primeiro o carrossel da Assembleia Geral Ordinaria

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -6,6 +6,43 @@ export default function Garbage() {
   return (
     <div className="carousel-inner">
       <Carousel>
+        {/* Inicio - by TC Fabio */}
+        <Carousel.Item>
+          <Link
+            target="_blank"
+            href="/documentos/Edital-AGO-eleicao-2026-2028.pdf"
+          >
+            <Image
+              alt="Assembléia Geral Ordinária"
+              src="/images/carrossel5.png"
+              width={1000}
+              height={1000}
+              priority={true}
+            />
+          </Link>
+          <div className="galleria-info">
+            <div className="galleria-info-text">
+              <div className="galleria-info-title">
+                <h3>
+                  <Link
+                    target="_blank"
+                    href="/documentos/Edital-AGO-eleicao-2026-2028.pdf"
+                  >
+                    Assembléia Geral Ordinária
+                  </Link>
+                </h3>
+              </div>
+              <div className="galleria-info-description">
+                Será realizada em duas modalidades, PRESENCIAL E VIRTUAL, pelo
+                aplicativo Meet. O link será disponibilizado oportunamente.
+                Confira o Edital.
+              </div>
+              {/* <div data-index={0} style={{ display: 'block' }} className="rights">
+                     Nome do autora da imagem</div> */}
+            </div>
+          </div>
+        </Carousel.Item>
+        {/* Fim - by TC Fabio */}
         <Carousel.Item>
           <Link
             target="_blank"
@@ -104,42 +141,6 @@ export default function Garbage() {
               </div>
               {/* <div data-index={0} style={{ display: 'block' }} className="rights">
                      Nome do autor da imagem</div> */}
-            </div>
-          </div>
-        </Carousel.Item>
-
-        <Carousel.Item>
-          <Link
-            target="_blank"
-            href="/documentos/Edital-AGO-eleicao-2026-2028.pdf"
-          >
-            <Image
-              alt="Assembléia Geral Ordinária"
-              src="/images/carrossel5.png"
-              width={1000}
-              height={1000}
-              priority={true}
-            />
-          </Link>
-          <div className="galleria-info">
-            <div className="galleria-info-text">
-              <div className="galleria-info-title">
-                <h3>
-                  <Link
-                    target="_blank"
-                    href="/documentos/Edital-AGO-eleicao-2026-2028.pdf"
-                  >
-                    Assembléia Geral Ordinária
-                  </Link>
-                </h3>
-              </div>
-              <div className="galleria-info-description">
-                Será realizada em duas modalidades, PRESENCIAL E VIRTUAL, pelo
-                aplicativo Meet. O link será disponibilizado oportunamente.
-                Confira o Edital.
-              </div>
-              {/* <div data-index={0} style={{ display: 'block' }} className="rights">
-                     Nome do autora da imagem</div> */}
             </div>
           </div>
         </Carousel.Item>
