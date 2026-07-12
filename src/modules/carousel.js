ecebb58407d78684cfa3b3d9b13cbc67d419c50b
AsEFEx <site.asefex@gmail.com>
2026-07-12 17:22:27 -0300

    Inclusão do Banner da Revista EsEFEx 131

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -41,6 +41,73 @@ export default function Garbage() {
             </div>
           </div>
         </Carousel.Item>
+        <Carousel.Item>
+          <Link
+            target="_blank"
+            href="https://asefex.com.br/diretoria-executiva"
+          >
+            <Image
+              alt="Diretoria AsEFEx 2026 - 2028"
+              src="/images/DiretoriaExecutiva2026.png"
+              width={1000}
+              height={1000}
+              priority={true}
+            />
+          </Link>
+          <div className="galleria-info">
+            <div className="galleria-info-text">
+              <div className="galleria-info-title">
+                <h3>
+                  <Link
+                    target="_blank"
+                    href="https://asefex.com.br/diretoria-executiva"
+                  >
+                    Diretoria Executiva AsEFEx
+                  </Link>
+                </h3>
+              </div>
+              <div className="galleria-info-description">
+                Equipe para o biênio 2026 - 2028.
+              </div>
+              {/* <div data-index={0} style={{ display: 'block' }} className="rights">
+                     Nome do autor da imagem</div> */}
+            </div>
+          </div>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <Link
+            target="_blank"
+            href="https://www.calameo.com/read/006435344eedc419c5a29"
+          >
+            <Image
+              alt="Revista AsEFEx 131"
+              src="/images/AsEFEx Notícias 131 - carrossel.png"
+              width={1000}
+              height={1000}
+              priority={true}
+            />
+          </Link>
+          <div className="galleria-info">
+            <div className="galleria-info-text">
+              <div className="galleria-info-title">
+                <h3>
+                  <Link
+                    target="_blank"
+                    href="https://www.calameo.com/read/006435344eedc419c5a29"
+                  >
+                    Informativo AsEFEx
+                  </Link>
+                </h3>
+              </div>
+              <div className="galleria-info-description">
+                Confira as matérias da Edição 131 da nossa revista
+              </div>
+              {/* <div data-index={0} style={{ display: 'block' }} className="rights">
+                     Nome do autor da imagem</div> */}
+            </div>
+          </div>
+        </Carousel.Item>
 
         <Carousel.Item>
           <Link
@@ -76,73 +143,7 @@ export default function Garbage() {
           </div>
         </Carousel.Item>
 
-        <Carousel.Item>
-          <Link
-            target="_blank"
-            href="https://www.calameo.com/read/0064353445805a31fe524"
-          >
-            <Image
-              alt="Revista AsEFEx 129"
-              src="/images/carrossel-Ed129.png"
-              width={1000}
-              height={1000}
-              priority={true}
-            />
-          </Link>
-          <div className="galleria-info">
-            <div className="galleria-info-text">
-              <div className="galleria-info-title">
-                <h3>
-                  <Link
-                    target="_blank"
-                    href="https://www.calameo.com/read/0064353445805a31fe524"
-                  >
-                    Informativo AsEFEx
-                  </Link>
-                </h3>
-              </div>
-              <div className="galleria-info-description">
-                Confira as matérias da Edição 129 da nossa revista.
-              </div>
-              {/* <div data-index={0} style={{ display: 'block' }} className="rights">
-                     Nome do autor da imagem</div> */}
-            </div>
-          </div>
-        </Carousel.Item>
         {/* Fim - by TC Fabio */}
-        <Carousel.Item>
-          <Link
-            target="_blank"
-            href="https://asefex.com.br/diretoria-executiva"
-          >
-            <Image
-              alt="Diretoria AsEFEx 2026 - 2028"
-              src="/images/DiretoriaExecutiva2026.png"
-              width={1000}
-              height={1000}
-              priority={true}
-            />
-          </Link>
-          <div className="galleria-info">
-            <div className="galleria-info-text">
-              <div className="galleria-info-title">
-                <h3>
-                  <Link
-                    target="_blank"
-                    href="https://asefex.com.br/diretoria-executiva"
-                  >
-                    Diretoria Executiva AsEFEx
-                  </Link>
-                </h3>
-              </div>
-              <div className="galleria-info-description">
-                Equipe para o biênio 2026 - 2028.
-              </div>
-              {/* <div data-index={0} style={{ display: 'block' }} className="rights">
-                     Nome do autor da imagem</div> */}
-            </div>
-          </div>
-        </Carousel.Item>
       </Carousel>
     </div>
   );
