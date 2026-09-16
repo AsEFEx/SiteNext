e75fda0ad20d5981c2f95c9eb0744b9f6d6c2558
AsEFEx <site.asefex@gmail.com>
2026-09-15 21:36:39 -0300

    AsEFEx - Cont... Corrigindo FormValidacao.

--- src/pages/api/validar.js
+++ src/pages/api/validar.js
@@ -1,6 +1,8 @@
 // pages/api/validar.js
 
 export default async function handler(req, res) {
+  
+  
   if (req.method !== 'POST') {
     return res.status(405).json({ erro: 'Método não permitido' });
   }
